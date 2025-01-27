04b565b73950d77ce2c2cabba1f33159da24b6e3
06051989abcd471loaD <dmitriy.avseyko@nbcuni.com>
2025-01-27 13:10:14 +0300

    homework_4 done_updated

--- homework/dmitriy_avseiko/Homework_4/task_1.py
+++ homework/dmitriy_avseiko/Homework_4/task_1.py
@@ -7,7 +7,7 @@ my_dict['set'] = {16, 17, 18, 19, 20}
 print(my_dict['tuple'][-1])
 my_dict['list'].append(21)
 my_dict['list'].pop(1)
-my_dict['dict']['i am a tuple'] = 'value6'
+my_dict['dict'][('i am a tuple',)] = 'value6'
 my_dict['dict'].pop('key1')
 my_dict['set'].add(22)
 my_dict['set'].remove(16)
