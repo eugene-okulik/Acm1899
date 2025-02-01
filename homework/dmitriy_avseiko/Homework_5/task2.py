dbc0f295c382e1ba3ccb582a7517b34b96edfa9d
06051989abcd471loaD <dmitriy.avseyko@nbcuni.com>
2025-02-02 00:40:49 +0300

    homework_5 done

--- homework/dmitriy_avseiko/Homework_5/task2.py
+++ homework/dmitriy_avseiko/Homework_5/task2.py
@@ -1,14 +1,14 @@
 result1 = 'результат операции: 42'
 index1 = result1.index(': ')
-num1 = int(result1[index1+2:])
+num1 = int(result1[index1 + 2:])
 print(num1 + 10)
 
 result2 = 'результат операции: 514'
 index2 = result2.index(': ')
-num2 = int(result2[index2+2:])
+num2 = int(result2[index2 + 2:])
 print(num2 + 10)
 
 result3 = 'результат работы программы: 9'
 index3 = result3.index(': ')
-num3 = int(result3[index3+2:])
+num3 = int(result3[index3 + 2:])
 print(num3 + 10)
