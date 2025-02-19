08bfa8f7499bce51aaa3f2fdcbd8aed6abd76a08
06051989abcd471loaD <dmitriy.avseyko@nbcuni.com>
2025-02-19 18:48:50 +0300

    Homework_12 done

--- homework/dmitriy_avseiko/Homework_12/task1.py
+++ homework/dmitriy_avseiko/Homework_12/task1.py
@@ -30,9 +30,10 @@ class Chamomile(Flower):
         self.is_medicinal = is_medicinal
 
 
-flower1 = Rose('rose',15, 7, 1, 'red', 50, 'honey')
-flower2 = Tulip('tulip',11, 10, 3, 'yellow', 30, True)
-flower3 = Chamomile('chamomile',20, 5, 2, 'white', 20, False)
+flower1 = Rose('rose', 15, 7, 1, 'red', 50, 'honey')
+flower2 = Tulip('tulip', 11, 10, 3, 'yellow', 30, True)
+flower3 = Chamomile('chamomile', 20, 5, 2, 'white', 20, False)
+
 
 class Bouquet:
 
@@ -41,31 +42,27 @@ class Bouquet:
         self.bouquet_price = self.calc_bouquet_price()
         self.bouquet_lifetime = self.calc_bouquet_lifetime()
 
-
     def calc_bouquet_price(self):
         bouquet_price = 0
         for i in self.flower_list:
             bouquet_price = bouquet_price + i.price
         return bouquet_price
 
-
     def calc_bouquet_lifetime(self):
         lifetime_list = [i.lifetime for i in self.flower_list]
         bouquet_lifetime = sum(lifetime_list) / len(lifetime_list)
         return bouquet_lifetime
-
 
     def sort_by_parametr(self, parametr):
         if parametr not in ['name', 'price', 'lifetime', 'age', 'color', 'length']:
             return 'Invalid parametr'
         flower_dict = {i.name: getattr(i, parametr, None) for i in self.flower_list}
         lst_tuples = []
-        for item  in flower_dict.items():
+        for item in flower_dict.items():
             lst_tuples.append(item)
         sort_lst_tuples = sorted(lst_tuples, key = lambda x: x[1])
         sort_by_param_lst = [i[0] for i in sort_lst_tuples]
         return ', '.join(sort_by_param_lst)
-
 
     def find_param_value(self, param, param_value):
         param_dict = {i.name: getattr(i, param, None) for i in self.flower_list}
