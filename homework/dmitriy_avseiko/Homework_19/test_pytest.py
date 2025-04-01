ef875597c49ab90731600eff9f1313aa0d31edaa
06051989abcd471loaD <dmitriy.avseyko@nbcuni.com>
2025-04-01 10:00:39 +0300

    Homework_19 done

--- homework/dmitriy_avseiko/Homework_19/test_pytest.py
+++ homework/dmitriy_avseiko/Homework_19/test_pytest.py
@@ -50,7 +50,7 @@ def test_get_object(get_object_id, conditions):
 
 @pytest.mark.parametrize('body', [{"name": "Test object1", "data": {"color": "red", "size": "1"}},
                                   {"name": "Test object2", "data": {"color": "blue", "size": "2"}},
-                                  {"name": "Test object3","data": {"color": "white","size": "3"}}])
+                                  {"name": "Test object3", "data": {"color": "white", "size": "3"}}])
 def test_create_an_object(conditions, body):
     headers = {'Content-Type': 'application/json'}
     response = requests.post(
